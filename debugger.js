1c1bee9f473cfdacbaa36f237671734753a63425
Mike Weber <mike@weberapps.com>
2013-10-23 11:58:35 -0500

    refactor debugger to be easily extended

--- debugger.js
+++ debugger.js
@@ -1,39 +1,65 @@
-window.Snow.Debugger = (function() {
-  function Debugger() {
+window.Snow.SnowDebugger = (function() {
+  function SnowDebugger() {
     this.def_list = document.createElement('dl')
-    this.flake_length_node = document.createElement('dd')
-    this.flake_length_node.appendChild(document.createTextNode(''))
-    this.fallen_flake_length_node = document.createElement('dd')
-    this.fallen_flake_length_node.appendChild(document.createTextNode(''))
+    this.def_list.setAttribute('id', 'debug')
+    this.nodes = {}
 
-    var dt2 = document.createElement('dt')
-    dt2.innerHTML = 'Fallen Flake size'
-    this.def_list.appendChild(this.fallen_flake_length_node)
-    this.def_list.appendChild(dt2)
-
-    var dt1 = document.createElement('dt')
-    dt1.appendChild(document.createTextNode('Flake size'))
-    this.def_list.appendChild(this.flake_length_node)
-    this.def_list.appendChild(dt1)
+    this.addTracker('Flake Size')
+    this.addTracker('Fallen Flake Size')
+    this.addTracker('FPS')
+    this.addTracker('Min Height')
 
     document.body.appendChild(this.def_list)
   }
 
   (function(klass) {
-    klass.prototype.updateStats = function() {
-      this.updateFlakeLength()
-      this.updateFallenFlakeLength()
+    klass.prototype.addTracker = function(title) {
+      var dt = document.createElement('dt')
+      var dd = document.createElement('dd')
+      this.nodes[title] = dd
+
+      dt.appendChild(document.createTextNode(title))
+      dd.appendChild(document.createTextNode(''))
+
+      this.def_list.appendChild(dt)
+      this.def_list.appendChild(dd)
     }
 
-    klass.prototype.updateFlakeLength = function() {
-      this.flake_length_node.innerHTML = flakes.length
+    klass.prototype.updateStats = function(renderer) {
+      this.updateFlakeLength(renderer)
+      this.updateFallenFlakeLength(renderer)
+      this.updateFPS(renderer)
+      this.updateMinHeight(renderer)
     }
 
-    klass.prototype.updateFallenFlakeLength = function() {
-      this.fallen_flake_length_node.innerHTML = fallen_flakes.length
+    klass.prototype.updateFlakeLength = function(renderer) {
+      this.nodes['Flake Size'].innerHTML = renderer.flakes.length
     }
-  })
 
-  return Debugger
+    klass.prototype.updateFallenFlakeLength = function(renderer) {
+      this.nodes['Fallen Flake Size'].innerHTML = renderer.fallen_flakes.length
+    }
+
+    klass.prototype.updateFPS = function(renderer) {
+      this.nodes['FPS'].innerHTML = Math.round(1000 / this.fps(renderer))
+    }
+
+    klass.prototype.fps = function(renderer) {
+      var dt = new Date() - renderer.last_draw
+      if (!this.weighted_dt) {
+        this.weighted_dt = dt
+      } else {
+        this.weighted_dt = this.weighted_dt * 0.9 + dt * 0.1
+      }
+
+      return this.weighted_dt
+    }
+
+    klass.prototype.updateMinHeight = function(renderer) {
+      this.nodes['Min Height'].innerHTML = renderer.fallen_flakes.snowCover()
+    }
+  })(SnowDebugger)
+
+  return SnowDebugger
 })()
 
